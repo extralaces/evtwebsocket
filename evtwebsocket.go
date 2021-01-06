302fe4f7ada75291411e9b3b75c04914d46465ba
bk <38148557+extralaces@users.noreply.github.com>
2021-01-06 15:45:38 -0600

    final update buffer size

--- evtwebsocket.go
+++ evtwebsocket.go
@@ -53,7 +53,7 @@ func (c *Conn) Dial(url, subprotocol string) error {
 		defer c.close()
 
 		for {
-			var msg = make([]byte, 2000)
+			var msg = make([]byte, 4000)
 			var n int
 			if n, err = c.ws.Read(msg); err != nil {
 				if c.OnError != nil {
